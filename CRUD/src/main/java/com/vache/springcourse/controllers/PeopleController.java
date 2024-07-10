8b9497b28d3ad9b0ebcb6f240119bca2650ede02
Vaache <hovhnanisyan@mail.ru>
2024-07-10 21:57:38 +0400

    CRUD

---
+++ CRUD/src/main/java/com/vache/springcourse/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package com.vache.springcourse.controllers;
+
+import com.vache.springcourse.dao.PersonDAO;
+import com.vache.springcourse.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+	private final PersonDAO personDAO;
+
+	@Autowired
+	public PeopleController(PersonDAO personDAO) {
+		this.personDAO = personDAO;
+	}
+
+
+	// Получим всех людей из DAO и передадим на отоброжения в представления
+	@GetMapping()
+	public String index(Model model) {
+		model.addAttribute( "people", personDAO.index());
+		return "people/index";
+	}
+
+	// Получим одного человека по id из DAO и передадим на отоброжения в представления
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") int id, Model model) {
+		model.addAttribute("person", personDAO.show(id));
+		return "people/show";
+	}
+}
